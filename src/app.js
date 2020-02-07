a66edf8054085c629deaa7fbd96ec9b345cfa295
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-02-07 16:02:08 -0200

    terminada base do servidor de items warframe

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Wolrd!!");
+const Items = require("warframe-items");
+
+const primary = new Items({ category: ["Primary"] });
+const pets = new Items({ category: ["Pets"] });
+const mods = new Items({ category: ["Mods"] });
+
+app.get("/api/warframe/pets", (req, res) => {
+  res.send(pets);
+});
+
+app.get("/api/warframe/mods", (req, res) => {
+  res.send(mods);
+});
+
+app.get("/api/warframe/primary", (req, res) => {
+  res.send(primary);
 });
 
 app.listen(3000);
