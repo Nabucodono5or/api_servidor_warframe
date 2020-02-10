5663cf3f7b44bb573a7a7eab8f19ad94bcd08207
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-02-10 00:54:23 -0200

    inserido listas de todos os itens

--- src/app.js
+++ src/app.js
@@ -3,9 +3,28 @@ const app = express();
 
 const Items = require("warframe-items");
 
+const all = new Items();
 const primary = new Items({ category: ["Primary"] });
 const pets = new Items({ category: ["Pets"] });
 const mods = new Items({ category: ["Mods"] });
+const arcanes = new Items({ category: ["Arcanes"] });
+const archwing = new Items({ category: ["Archwing"] });
+const archGun = new Items({ category: ["Arch-Gun"] });
+const archMelee = new Items({ category: ["Arch-Melee"] });
+const enemy = new Items({ category: ["Enemy"] });
+const fish = new Items({ category: ["Fish"] });
+const gear = new Items({ category: ["Gear"] });
+const glyphs = new Items({ category: ["Glyphs"] });
+const melee = new Items({ category: ["Melee"] });
+const misc = new Items({ category: ["Misc"] });
+const quests = new Items({ category: ["Quests"] });
+const relics = new Items({ category: ["Relics"] });
+const resources = new Items({ category: ["Resources"] });
+const secondary = new Items({ category: ["Secondary"] });
+const sentinels = new Items({ category: ["Sentinels"] });
+const sigils = new Items({ category: ["Sigils"] });
+const skins = new Items({ category: ["Skins"] });
+const warframes = new Items({ category: ["Warframes"] });
 
 app.get("/api/warframe/pets", (req, res) => {
   res.send(pets);
